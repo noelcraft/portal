c300ce95f1e4361aaecc254d85540931a644ca45
Gdeft <63198340+Gdeft@users.noreply.github.com>
2020-10-11 17:59:08 +0700

    Startup Dialog

--- src/portal/Portal.java
+++ src/portal/Portal.java
@@ -6,17 +6,32 @@ import mindustry.*;
 import mindustry.content.*;
 import mindustry.game.EventType.*;
 import mindustry.gen.*;
+import mindustry.graphics.Pal;
 import mindustry.mod.*;
 import mindustry.ui.dialogs.*;
 
 public class Portal extends Mod{
+	public final String githubURL = "https://github.com/Gdeft/portal";
 
 	public Portal(){
 		Log.info("Loaded Portal constructor.");
 
 		Events.on(ClientLoadEvent.class, e -> {
 			Time.runTask(10f, () -> {
-				Vars.ui.showInfo(Core.bundle.get("dialog.portal.info"));
+				BaseDialog dialog = new BaseDialog("@dialog.startup.title");
+				
+				dialog.cont.pane(p -> {
+					p.add("Portal").center().color(Pal.accent).row();
+					p.image().pad(4f).height(6f).fillX().color(Pal.accent).row();
+					p.labelWrap("@dialog.startup.info").pad(4f).growX().row();
+					
+					p.button("@dialog.startup.github", Icon.github, () -> {
+						Core.app.openURI(githubURL);
+					}).growX().pad(16f).size(210f, 64f).center();
+				}).growY().width(540).center().row();
+				
+				dialog.cont.button("@ok", dialog::hide).size(105f, 55f).center();
+				dialog.show();
 			});
 		});
 	}
